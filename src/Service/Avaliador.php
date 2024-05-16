330a79b023e9a44591ffcd2af69e84429f22bed4
Eric Araújo Lima <eric.araujo@apoioprodesp.sp.gov.br>
2024-05-16 17:41:02 -0300

    Capturando os 3 maiores lances

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,16 +2,20 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private float $maiorValor = -INF;
     private float $menorValor = INF;
+    private array $maioresLances = [];
 
     public function avaliar(Leilao $leilao): void
     {
-        foreach ($leilao->getLances() as $lance) {
+        $lances = $leilao->getLances();
+
+        foreach ($lances as $lance) {
             if ($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
             }
@@ -20,6 +24,12 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
@@ -30,5 +40,10 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -81,4 +81,36 @@ class AvaliadorTest extends TestCase
             actual: $menorValor
         );
     }
+
+    public function testAvaliadorDeveBuscarOs3MaioresValores(): void
+    {
+        $usuario1 = new Usuario("Eric");
+        $usuario2 = new Usuario("Renan");
+        $usuario3 = new Usuario("Rodrigo");
+        $usuario4 = new Usuario("Douglas");
+
+        $leilao = new Leilao("Supra 95 2JZ");
+        $leilao->recebeLance(
+            new Lance($usuario1, 20000)
+        );
+        $leilao->recebeLance(
+            new Lance($usuario2, 5000)
+        );
+        $leilao->recebeLance(
+            new Lance($usuario3, 15000)
+        );
+        $leilao->recebeLance(
+            new Lance($usuario4, 1000)
+        );
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avaliar($leilao);
+
+        $maioresLances = $leiloeiro->getMaioresLances();
+
+        self::assertCount(3, $maioresLances);
+        self::assertEquals(20000, $maioresLances[0]->getValor());
+        self::assertEquals(15000, $maioresLances[1]->getValor());
+        self::assertEquals(5000, $maioresLances[2]->getValor());
+    }
 }
